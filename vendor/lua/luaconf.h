9de77ce4618f480c6ec92f6020583b2b5ee9dff7
Eduardo Tongson <propolice@gmail.com>
2015-04-18 13:59:25 +0800

    lua: We don't define LUA_USE_LINUX anymore so back to vanilla luaconf.h.

--- vendor/lua/luaconf.h
+++ vendor/lua/luaconf.h
@@ -60,6 +60,8 @@
 
 #if defined(LUA_USE_LINUX)
 #define LUA_USE_POSIX
+#define LUA_USE_DLOPEN          /* needs an extra library: -ldl */
+#define LUA_USE_READLINE        /* needs some extra libraries */
 #endif
 
 
