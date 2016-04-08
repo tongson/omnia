169d561db6701a1930f5e0ba6b11859aa4e7bef1
Eduardo Tongson <propolice@gmail.com>
2016-04-08 09:40:37 +0800

    Add support for including plain Lua modules in subdirectories. Luastatic already has support for this so take advantage of it. VENDOR_SUBDIRS are in vendor/lua and VENDOR_DEPS can be deduced from rockspecs. Example (LDoc): VENDOR_SUBDIRS= pl ldoc VENDOR_DEPS+= pl/app.lua pl/array2d.lua pl/class.lua pl/compat.lua pl/comprehension.lua pl/config.lua pl/data.lua pl/Date.lua pl/dir.lua pl/file.lua pl/func.lua pl/import_into.lua pl/init.lua pl/input.lua pl/lapp.lua pl/lexer.lua pl/List.lua pl/luabalanced.lua pl/Map.lua pl/MultiMap.lua pl/operator.lua pl/OrderedMap.lua pl/path.lua pl/permute.lua pl/pretty.lua pl/seq.lua pl/Set.lua pl/sip.lua pl/strict.lua pl/stringio.lua pl/stringx.lua pl/tablex.lua pl/template.lua pl/test.lua pl/text.lua pl/types.lua pl/url.lua pl/utils.lua pl/xml.lua VENDOR_DEPS+= ldoc/builtin/coroutine.lua ldoc/builtin/lfs.lua ldoc/lang.lua ldoc/html.lua ldoc/builtin/table.lua ldoc/builtin/os.lua ldoc/doc.lua ldoc/html/ldoc_one_css.lua ldoc/builtin/string.lua ldoc/builtin/package.lua ldoc/builtin/math.lua ldoc/builtin/lpeg.lua ldoc/builtin/io.lua ldoc/builtin/debug.lua ldoc/builtin/global.lua ldoc/builtin/globals.lua ldoc/html/ldoc_ltp.lua ldoc/tools.lua ldoc/html/ldoc_css.lua ldoc/markup.lua ldoc/prettify.lua ldoc/parse.lua ldoc/lexer.lua

--- aux/rules.mk
+++ aux/rules.mk
@@ -6,9 +6,9 @@ $(LUA_T):
 	$(ECHOT) [CC] $@
 	$(CC) -o $@ -DMAKE_LUA $(luaDEFINES) $(INCLUDES) $(CCWARN) $(CFLAGS) $(CCOPT) $(ONE).c -lm
 
-$(LUA_O): $(BUILD_DEPS) $(LUA_T) 
+$(LUA_O): $(BUILD_DEPS) $(LUA_T)
 	$(ECHOT) [CC] $@
-	$(CC) -o $@ -c -DMAKE_LIB $(DEFINES) $(luaDEFINES) $(INCLUDES) $(CCWARN) $(CFLAGS) $(CCOPT) $(ONE).c 
+	$(CC) -o $@ -c -DMAKE_LIB $(DEFINES) $(luaDEFINES) $(INCLUDES) $(CCWARN) $(CFLAGS) $(CCOPT) $(ONE).c
 
 $(LUA_A): $(LUA_O)
 	$(ECHOT) [AR] $@
@@ -17,15 +17,15 @@ $(LUA_A): $(LUA_O)
 
 $(EXE): $(LUA_A) $(CLUA_MODS)
 	$(ECHOT) [CP] $(LUA_MODS)
-	cd $(MODULES_P) && \
-	$(CP) $(LUA_MODS) ../..		
+	for f in $(LUA_MODS); do cp $(MODULES_P)/$$f .; done
+	for d in $(VENDOR_SUBDIRS); do cp -R $(MODULES_P)/$$d .; done
 	$(ECHOT) [LN] $(MAIN)
-	$(LUA_T) $(LUASTATIC) $(MAIN) $(LUA_MODS) $(CLUA_MODS) $(LUA_A) $(INCLUDES) $(CCWARN) $(CFLAGS) $(CCOPT) $(LDFLAGS) 2>&1 >/dev/null
+	$(LUA_T) $(LUASTATIC) $(MAIN) $(LUA_MODS) $(VENDOR_DEPS) $(CLUA_MODS) $(LUA_A) $(INCLUDES) $(CCWARN) $(CFLAGS) $(CCOPT) $(LDFLAGS) 2>&1 >/dev/null
 
-clean: $(CLEAN) 
+clean: $(CLEAN)
 	$(ECHO) "Cleaning up..."
 	$(RM) $(RMFLAGS) $(LUA_O) $(LUA_T) $(LUAC_T) $(EXE) $(LUA_A) $(MAIN).c $(LUA_MODS)
-	$(RMRF) test/tmp
+	$(RMRF) $(VENDOR_SUBDIRS)
 	$(ECHO) "Done!"
 
 print-%: ; @echo $*=$($*)
