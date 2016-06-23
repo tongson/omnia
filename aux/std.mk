34eff8e8cb711c1ce9898331a625cd737c02d5d1
Eduardo Tongson <propolice@gmail.com>
2016-06-23 16:57:14 +0800

    build: missing directory /c

--- aux/std.mk
+++ aux/std.mk
@@ -33,7 +33,7 @@ LUA_MODS+= $(foreach m, $(VENDOR_LUA), $m.lua)
 LUA_MODS+= $(foreach m, $(APP_LUA), $m.lua)
 BUILD_DEPS= has-$(CC) has-$(RANLIB) has-$(LD) has-$(AR) has-$(STRIP) has-$(RM) has-$(CP)
 ifneq ($(APP_C),)
-  include $(eval _d:=app/$(APP_C) $(_d)) $(call _lget,$(APP_C))
+  include $(eval _d:=app/c/$(APP_C) $(_d)) $(call _lget,$(APP_C))
 endif
 ifneq ($(VENDOR_C),)
   include $(eval _d:=vendor/c/$(VENDOR_C) $(_d)) $(call _vget,$(VENDOR_C))
