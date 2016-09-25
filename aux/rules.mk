0e8b64fa6883f92de1e3eeebd7b3d1bacc05b6c0
Eduardo Tongson <propolice@gmail.com>
2016-09-25 10:49:27 +0800

    build: Remove rm -rf for these directories. Already done in the EXE_T recipe.

--- aux/rules.mk
+++ aux/rules.mk
@@ -38,5 +38,4 @@ $(EXE_T): $(BUILD_DEPS) $(LUA_A) $(C_MODULES) $(COMPILED) $(MODULES) $(SRC_LUA) 
 clean: $(CLEAN)
 	$(ECHO) "Cleaning up..."
 	$(RM) $(RMFLAGS) $(LUA_O) $(LUA_T) $(LUAC_T) $(LUA_A) $(MAIN).c $(EXE_T) $(MODULES)
-	$(RMRF) $(VENDOR_DIRS) $(SRC_DIRS)
 	$(ECHO) "Done!"
