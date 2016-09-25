eda564b7ef05f7c543879885dbeed152d05a4167
Eduardo Tongson <propolice@gmail.com>
2016-09-25 09:56:37 +0800

    build: Tidy up. Split out EXE_T target recipe.

--- aux/rules.mk
+++ aux/rules.mk
@@ -15,12 +15,20 @@ $(LUA_A): $(LUA_O)
 	$(AR) $(ARFLAGS) $@ $< >/dev/null 2>&1
 	$(RANLIB) $@
 
-$(EXE_T): $(BUILD_DEPS) $(LUA_A) $(C_MODULES) $(COMPILED)
+$(MODULES):
 	$(ECHOT) [CP] $(MODULES)
 	for f in $(VENDOR); do cp $(VENDOR_P)/$$f.lua .; done
 	for f in $(SRC); do cp $(SRC_P)/$$f.lua .; done
-	for d in $(VENDOR_DIRS); do cp -R $(VENDOR_P)/$$d .; done
+
+$(SRC_LUA):
+	$(ECHOT) [CP] $(SRC_DIRS)
 	for d in $(SRC_DIRS); do cp -R $(SRC_P)/$$d .; done
+
+$(VENDOR_LUA):
+	$(ECHOT) [CP] $(VENDOR_DIRS)
+	for d in $(VENDOR_DIRS); do cp -R $(VENDOR_P)/$$d .; done
+
+$(EXE_T): $(BUILD_DEPS) $(LUA_A) $(C_MODULES) $(COMPILED) $(MODULES) $(SRC_LUA) $(VENDOR_LUA)
 	$(ECHOT) [LN] $(EXE_T)
 	CC=$(CC) NM=$(NM) $(LUA_T) $(LUASTATIC) $(MAIN) $(SRC_LUA) $(VENDOR_LUA) $(MODULES) $(C_MODULES) \
 		 $(LUA_A) $(INCLUDES) $(CCWARN) $(CFLAGS) $(CCOPT) $(LDFLAGS) 2>&1 >/dev/null
