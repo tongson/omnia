2f454de2cc7f930ca9b96b72d1a5cd75790f4a40
Eduardo Tongson <propolice@gmail.com>
2016-10-01 09:55:56 +0800

    Update Moonscript to 0.5.0.

--- vendor/lua/moonscript/version.lua
+++ vendor/lua/moonscript/version.lua
@@ -1,1 +1,1 @@
-local version="0.4.0"return{version=version,print_version=function()return print("MoonScript version "..tostring(version))end}
+local version="0.5.0"return{version=version,print_version=function()return print("MoonScript version "..tostring(version))end}
