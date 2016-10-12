bf6797abf1c70508f4ebb8a25c3f380278725d5b
Eduardo Tongson <propolice@gmail.com>
2016-10-12 10:22:13 +0800

    Initial import of moonpick. 2e390b34ceb517cdc22e55e3527e615d0689d0be.

--- aux/moonscript.mk
+++ aux/moonscript.mk
@@ -1,7 +1,9 @@
 MOONC_T= bin/moonc
 MOONI_T= bin/mooni
+MOONPICK_T= bin/moonpick
 MOONC= bin/moonc.lua
 MOONI= bin/mooni.lua
+MOONPICK= bin/moonpick.lua
 MOONSCRIPT= moonscript/*.lua moonscript/parse/*.lua moonscript/compile/*.lua moonscript/transform/*.lua
 CLEAN+= clean_moonscript
 
@@ -21,8 +23,18 @@ $(MOONI_T): $(MOONC_T)
 	$(RM) $(RMFLAGS) cimicida.lua $(MOONI).c
 	$(RMRF) moonscript
 
+$(MOONPICK_T): $(HOST_LUA_A) $(LUA_T) $(host_lpegA)
+	$(ECHOT) CC $@
+	$(CPR) vendor/lua/moonpick vendor/lua/moonscript .
+	$(CP) vendor/lua/moonpick.lua .
+	CC=$(HOST_CC) NM=$(NM) $(LUA_T) $(LUASTATIC) $(MOONPICK) moonpick.lua moonpick/*.lua $(MOONSCRIPT) $(host_lpegA) \
+		 $(HOST_LUA_A) $(FLAGS) $(LDFLAGS) 2>&1 >/dev/null
+	$(RM) $(RMFLAGS) moonpick.lua $(MOONPICK).c
+	$(RMRF) moonpick moonscript
+
+
 %.lua: $(MOONC_T) %.moon
 	$(MOONC_T) $*.moon $@
 
 clean_moonscript:
-	$(RM) $(RMFLAGS) $(COMPILED) $(MOONC_T) $(MOONI_T) $(host_lpegA) cimicida.lua
+	$(RM) $(RMFLAGS) $(COMPILED) $(MOONC_T) $(MOONI_T) $(MOONPICK_T) $(host_lpegA) cimicida.lua
