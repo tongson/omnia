c4a0199c2253de194beb0b9c22e79be58dbf85af
Eduardo Tongson <propolice@gmail.com>
2016-10-21 11:32:08 +0800

    Import factid from Configi.

---
+++ vendor/c/factid/src/factidC.c
@@ -0,0 +1,556 @@
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/utsname.h>
+#include <limits.h>
+#include <time.h>
+#include <locale.h>
+#include <mntent.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <ifaddrs.h>
+#include <linux/if_link.h>
+#ifndef _LINUX_SYSINFO_H
+# include <sys/sysinfo.h>
+#endif
+#ifdef HAVE_UCLIBC
+# include <utmp.h>
+# define utmpx utmp
+# define utmxpname utmpname
+# define setutxent setutent
+# define getutxent getutent
+# define endutxent endutent
+# define getutxline getutline
+# define getutxid getutid
+# define pututxline pututline
+#else
+# include <utmpx.h>
+#endif
+
+#include <assert.h>
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
+#ifndef _UTSNAME_LENGTH
+#define _UTSNAME_LENGTH 65
+#define _UTSNAME_DOMAIN_LENGTH 65
+#endif
+
+static int pusherror(lua_State *L, char *error)
+{
+	lua_pushnil(L);
+	lua_pushstring(L, error);
+	return 2;
+}
+
+static int pusherrno(lua_State *L, char *error)
+{
+	lua_pushnil(L);
+	lua_pushfstring(L, LUA_QS" : "LUA_QS, error, strerror(errno));
+	lua_pushinteger(L, errno);
+	return 3;
+}
+
+
+/*
+Uptime via sysinfo(2).
+@function uptime
+@return uptime (NUMBER)
+*/
+static int Fuptime(lua_State *L)
+{
+	struct sysinfo info = {0};
+
+	if (sysinfo(&info) == -1) return pusherrno(L, "sysinfo(2) error");
+	lua_createtable(L, 0, 5);
+	lua_pushinteger(L, info.uptime);
+	lua_setfield(L, -2, "totalseconds");
+	info.uptime /= 60;
+	lua_pushinteger(L, info.uptime);
+	lua_setfield(L, -2, "totalminutes");
+	lua_pushinteger(L, info.uptime%60);
+	lua_setfield(L, -2, "minutes");
+	info.uptime /= 60;
+	lua_pushinteger(L, info.uptime%24);
+	lua_setfield(L, -2, "hours");
+	lua_pushinteger(L, info.uptime/24);
+	lua_setfield(L, -2, "days");
+	return 1;
+}
+
+/*
+Load averages via sysinfo(2)
+@function loads
+@return load averages (TABLE)
+*/
+static int Floads(lua_State *L)
+{
+	int l;
+	struct sysinfo info = {0};
+
+	if (sysinfo(&info) == -1) return pusherrno(L, "sysinfo(2) error");
+	lua_createtable(L, 3, 0);
+	for (l = 0; l < 3; l++) {
+		lua_pushnumber(L, info.loads[l]/65536.00);
+		lua_rawseti(L, -2, l+1);
+	}
+	return 1;
+}
+
+/*
+Memory usage via sysinfo(2)
+@function mem
+@return memory usage (TABLE)
+*/
+static int Fmem(lua_State *L)
+{
+	struct sysinfo info = {0};
+
+	if (sysinfo(&info) == -1) return pusherrno(L, "sysinfo(2) error");
+	lua_createtable(L, 0, 9);
+	lua_pushinteger(L, info.mem_unit);
+	lua_setfield(L, -2, "mem_unit");
+	lua_pushinteger(L, info.freehigh);
+	lua_setfield(L, -2, "freehigh");
+	lua_pushinteger(L, info.totalhigh);
+	lua_setfield(L, -2, "totalhigh");
+	lua_pushinteger(L, info.freeswap);
+	lua_setfield(L, -2, "freeswap");
+	lua_pushinteger(L, info.totalswap);
+	lua_setfield(L, -2, "totalswap");
+	lua_pushinteger(L, info.bufferram);
+	lua_setfield(L, -2, "bufferram");
+	lua_pushinteger(L, info.sharedram);
+	lua_setfield(L, -2, "sharedram");
+	lua_pushinteger(L, info.freeram);
+	lua_setfield(L, -2, "freeram");
+	lua_pushinteger(L, info.totalram);
+	lua_setfield(L, -2, "totalram");
+	return 1;
+}
+
+/*
+Number of processes via sysinfo(2)
+@function procs
+@return number of processes (NUMBER)
+*/
+static int Fprocs(lua_State *L)
+{
+	struct sysinfo info = {0};
+
+	if (sysinfo(&info) == -1) return pusherrno(L, "sysinfo(2) error");
+	lua_pushinteger(L, info.procs);
+	return 1;
+}
+
+/*
+Show sysconf(3) information
+@function sysconf
+@return values (TABLE)
+*/
+static int Fsysconf(lua_State *L)
+{
+	struct {
+		char *name;
+		long sc;
+	} m[] = {
+		{"openmax", sysconf(_SC_OPEN_MAX)},
+		{"procs", sysconf(_SC_NPROCESSORS_CONF)},
+		{"procsonline", sysconf(_SC_NPROCESSORS_ONLN)},
+		{"pagesize", sysconf(_SC_PAGESIZE)},
+		{"physpages", sysconf(_SC_PHYS_PAGES)},
+		{"avphyspages", sysconf(_SC_AVPHYS_PAGES)}
+	};
+
+	lua_createtable(L, 0, 6);
+	int c;
+	for (c = 0; c < sizeof m/sizeof *m; c++) {
+		lua_pushinteger(L, m[c].sc);
+		lua_setfield(L, -2, m[c].name);
+	}
+	assert(c == 6);
+	return 1;
+}
+
+/*
+Hostname via gethostname(2)
+@function hostname
+@return hostname (STRING)
+*/
+static int Fhostname(lua_State *L)
+{
+	char hostname[1026]; // NI_MAXHOST + 1
+	if (!gethostname(hostname, (sizeof hostname)-1)) {
+		hostname[(sizeof hostname)-1] = '\0';
+		assert(hostname[1025] == '\0');
+		lua_pushstring(L, hostname);
+	} else {
+		return pusherrno(L, "gethostname(2) error");
+	}
+	return 1;
+}
+
+/*
+Show uname information via uname(2)
+@function uname
+@return information (TABLE)
+*/
+static int Funame(lua_State *L)
+{
+	struct utsname uts = {0};
+	char buf[_UTSNAME_LENGTH];
+	char dbuf[_UTSNAME_DOMAIN_LENGTH];
+
+	if (uname(&uts) == -1) return pusherrno(L, "uname(2) error");
+	lua_createtable(L, 0, 6);
+	memset(&buf, 0, _UTSNAME_LENGTH);
+	strncpy(buf, uts.sysname, _UTSNAME_LENGTH);
+	buf[_UTSNAME_LENGTH-1] = '\0';
+	lua_pushstring(L, buf);
+	lua_setfield(L, -2, "sysname");
+	memset(&buf, 0, _UTSNAME_LENGTH);
+	strncpy(buf, uts.nodename, _UTSNAME_LENGTH);
+	buf[_UTSNAME_LENGTH-1] = '\0';
+	lua_pushstring(L, buf);
+	lua_setfield(L, -2, "nodename");
+	memset(&buf, 0, _UTSNAME_LENGTH);
+	strncpy(buf, uts.release, _UTSNAME_LENGTH);
+	buf[_UTSNAME_LENGTH-1] = '\0';
+	lua_pushstring(L, buf);
+	lua_setfield(L, -2, "release");
+	memset(&buf, 0, _UTSNAME_LENGTH);
+	strncpy(buf, uts.version, _UTSNAME_LENGTH);
+	buf[_UTSNAME_LENGTH-1] = '\0';
+	lua_pushstring(L, buf);
+	lua_setfield(L, -2, "version");
+	memset(&buf, 0, _UTSNAME_LENGTH);
+	strncpy(buf, uts.machine, _UTSNAME_LENGTH);
+	buf[_UTSNAME_LENGTH-1] = '\0';
+	lua_pushstring(L, buf);
+	lua_setfield(L, -2, "machine");
+	memset(&dbuf, 0, _UTSNAME_DOMAIN_LENGTH);
+#ifdef _GNU_SOURCE
+	strncpy(dbuf, uts.domainname, _UTSNAME_DOMAIN_LENGTH);
+#else
+	strncpy(dbuf, uts.__domainname, _UTSNAME_DOMAIN_LENGTH);
+#endif
+	dbuf[_UTSNAME_DOMAIN_LENGTH-1] = '\0';
+	lua_pushstring(L, dbuf);
+	lua_setfield(L, -2, "domainname");
+	return 1;
+}
+
+/*
+Hostid via gethostid(3)
+@function hostid
+@return hostid (STRING)
+*/
+static int Fhostid(lua_State *L)
+{
+	char hostid[9];
+
+	snprintf(hostid, sizeof hostid, "%08lx", gethostid());
+	assert(hostid[8] == '\0');
+	lua_pushstring(L, hostid);
+	return 1;
+}
+
+/*
+Current timezone via strftime(3)
+@function timezone
+@return timezone (STRING)
+*/
+static int Ftimezone(lua_State *L)
+{
+	char tzbuf[4];
+        struct tm *te = {0};
+        time_t t;
+        t = time(NULL);
+        te = localtime(&t);
+
+        setlocale(LC_TIME, "C");
+	if (!strftime(tzbuf, sizeof tzbuf, "%Z", te)) return pusherror(L, "strftime(3) error");
+	assert(tzbuf[3] == '\0');
+	lua_pushstring(L, tzbuf);
+	return 1;
+}
+
+/*
+Mount information via getmntent(3)
+@function mount
+@return mount information (TABLE)
+*/
+static int Fmount(lua_State *L)
+{
+	int c = 0;
+	struct mntent *m = {0};
+	FILE *mtab = setmntent("/etc/mtab", "r");
+
+	if (!mtab) mtab = setmntent("/proc/self/mounts", "r");
+	if (!mtab) return pusherrno(L, "setmntent(3) error");
+	if (setvbuf(mtab, (void *)0, _IONBF, 0)) return pusherrno(L, "setvbuf(3) error");
+	lua_newtable(L);
+	while ((m = getmntent(mtab))) {
+		lua_createtable(L, 0, 6);
+		lua_pushfstring(L, "%s", m->mnt_fsname);
+		lua_setfield(L, -2, "fsname");
+		lua_pushfstring(L, "%s", m->mnt_dir);
+		lua_setfield(L, -2, "dir");
+		lua_pushfstring(L, "%s", m->mnt_type);
+		lua_setfield(L, -2, "type");
+		lua_pushfstring(L, "%s", m->mnt_opts);
+		lua_setfield(L, -2, "opts");
+		lua_pushinteger(L, m->mnt_freq);
+		lua_setfield(L, -2, "freq");
+		lua_pushinteger(L, m->mnt_passno);
+		lua_setfield(L, -2, "passno");
+		lua_rawseti(L, -2, c);
+		c++;
+	}
+	endmntent(mtab);
+	return 1;
+}
+
+/*
+Deduce outgoing IPv4 and IPv6 address
+@function ipaddress
+@return addresses (TABLE)
+*/
+static int Fipaddress(lua_State *L)
+{
+	int fd4, fd6, c;
+	char ipv6[INET6_ADDRSTRLEN];
+	char ipv4[INET_ADDRSTRLEN];
+	struct sockaddr_in l4 = {0}, r4 = {0}, ip4 = {0};
+	struct sockaddr_in6 l6 = {0}, r6 = {0}, ip6 = {0};
+	socklen_t ip4len = sizeof ip4;
+	socklen_t ip6len = sizeof ip6;
+
+	l4.sin_family = AF_INET;
+	l4.sin_port = htons(0);
+	l4.sin_addr.s_addr = htonl(INADDR_ANY);
+	r4.sin_family = AF_INET;
+	r4.sin_port = htons(40444);
+	inet_pton(AF_INET, "8.8.8.8", &r4.sin_addr.s_addr);
+
+	l6.sin6_family = AF_INET6;
+	l6.sin6_port = htons(0);
+	l6.sin6_addr = in6addr_any;
+	r6.sin6_family = AF_INET6;
+	r6.sin6_port = htons(40666);
+	inet_pton(AF_INET6, "2001:4860:4860::8888", &r6.sin6_addr);
+
+	lua_createtable(L, 0, 2);
+	if ((fd4 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) return pusherrno(L, "socket(2) error");
+	for (c = 0; c <= 3; c++) {
+		if (!connect(fd4, (struct sockaddr *)&r4, sizeof r4)) break;
+		if (c == 3) {
+			inet_pton(AF_INET, "127.0.0.1", &r4.sin_addr.s_addr);
+			if (connect(fd4, (struct sockaddr *)&r4, sizeof r4) == -1) return pusherrno(L, "connect(2) error");
+		}
+	}
+	if (getsockname(fd4, (struct sockaddr *)&ip4, &ip4len) == -1) return pusherrno(L, "getsockname(2) error");
+	shutdown(fd4, 2);
+	inet_ntop(AF_INET, &ip4.sin_addr, ipv4, INET_ADDRSTRLEN);
+	lua_pushstring(L, ipv4);
+	lua_setfield(L, -2, "ipv4");
+
+	if ((fd6 = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)) == -1) return pusherrno(L, "socket(2) error");
+	for (c = 0; c <= 3; c++) {
+		if (!connect(fd6, (struct sockaddr *)&r6, sizeof r6)) break;
+		if (c == 3) {
+			inet_pton(AF_INET6, "::1", &r6.sin6_addr);
+			if (connect(fd6, (struct sockaddr *)&r6, sizeof r6) == -1) {
+				shutdown(fd6, 2);
+				lua_pushstring(L, "disabled");
+				lua_setfield(L, -2, "ipv6");
+				return 1;
+			}
+		}
+	}
+	if (getsockname(fd6, (struct sockaddr *)&ip6, &ip6len) == -1) return pusherrno(L, "getsockname(2) error");
+	shutdown(fd6, 2);
+	inet_ntop(AF_INET6, &ip6.sin6_addr, ipv6, INET6_ADDRSTRLEN);
+	lua_pushstring(L, ipv6);
+	lua_setfield(L, -2, "ipv6");
+	return 1;
+}
+
+/*
+Network interface statistics
+@function ifstats
+@return stats (TABLE)
+*/
+static int ifstats(lua_State *L, struct ifaddrs *i)
+{
+	struct rtnl_link_stats *stats = i->ifa_data;
+	lua_createtable(L, 0, 10);
+	lua_pushinteger(L, stats->rx_packets);
+	lua_setfield(L, -2, "rx_packets");
+	lua_pushinteger(L, stats->tx_packets);
+	lua_setfield(L, -2, "tx_packets");
+	lua_pushinteger(L, stats->rx_bytes);
+	lua_setfield(L, -2, "rx_bytes");
+	lua_pushinteger(L, stats->tx_bytes);
+	lua_setfield(L, -2, "tx_bytes");
+	lua_pushinteger(L, stats->rx_errors);
+	lua_setfield(L, -2, "rx_errors");
+	lua_pushinteger(L, stats->tx_errors);
+	lua_setfield(L, -2, "tx_errors");
+	lua_pushinteger(L, stats->rx_dropped);
+	lua_setfield(L, -2, "rx_dropped");
+	lua_pushinteger(L, stats->tx_dropped);
+	lua_setfield(L, -2, "tx_dropped");
+	lua_pushinteger(L, stats->multicast);
+	lua_setfield(L, -2, "multicast");
+	lua_pushinteger(L, stats->collisions);
+	lua_setfield(L, -2, "collisions");
+	lua_setfield(L, -2, "data");
+	return 1;
+}
+
+static int ipaddr(lua_State *L, struct ifaddrs *i)
+{
+	char ipv4[INET_ADDRSTRLEN];
+	char ipv6[INET6_ADDRSTRLEN];
+	void *ip = 0;
+
+	if (i->ifa_addr->sa_family == AF_INET) {
+		ip = &((struct sockaddr_in *)i->ifa_addr)->sin_addr;
+		inet_ntop(AF_INET, ip, ipv4, INET_ADDRSTRLEN);
+		lua_pushstring(L, ipv4);
+		lua_setfield(L, -2, "ipv4");
+	}
+	if (i->ifa_addr->sa_family == AF_INET6) {
+		ip = &((struct sockaddr_in6 *)i->ifa_addr)->sin6_addr;
+		inet_ntop(AF_INET6, ip, ipv6, INET6_ADDRSTRLEN);
+		lua_pushstring(L, ipv6);
+		lua_setfield(L, -2, "ipv6");
+	}
+	return 1;
+}
+
+/*
+Show configured IP addresses.
+@function ifaddrs
+@return information (TABLE)
+*/
+static int Fifaddrs(lua_State *L)
+{
+	struct ifaddrs *ifaddr = {0};
+	struct ifaddrs *i = {0};
+	int c = 1;
+
+	if (getifaddrs(&ifaddr) == -1) return pusherrno(L, "getifaddrs(3) error");
+	lua_newtable(L);
+	for (i = ifaddr; i != 0 ; i = i->ifa_next) {
+		if (i->ifa_addr == 0) continue;
+		lua_newtable(L);
+		lua_pushstring(L, i->ifa_name);
+		lua_setfield(L, -2, "interface");
+		if (i->ifa_addr->sa_family == AF_INET || i->ifa_addr->sa_family == AF_INET6) ipaddr(L, i);
+		if (i->ifa_data && (i->ifa_addr->sa_family == AF_PACKET)) ifstats(L, i);
+		lua_rawseti(L, -2, c++);
+	}
+	freeifaddrs(ifaddr);
+	return 1;
+}
+
+/*
+Show MAC addresses
+@function macaddrs
+@return addresses (TABLE)
+*/
+static int Fmacaddrs(lua_State *L)
+{
+	char buf[8192];
+	char mac[18];
+	int fd = 0;
+	int c = 0;
+ 	struct ifconf ifc = {0};
+	struct ifreq *cifr = {0};
+
+	if ((fd = socket(PF_INET, SOCK_DGRAM, 0)) == -1) return pusherrno(L, "socket(2) error");
+	ifc.ifc_len = sizeof buf;
+	ifc.ifc_buf = buf;
+	if (ioctl(fd, SIOCGIFCONF, &ifc) == -1) return pusherrno(L, "ioctl(2) error");
+	lua_newtable(L);
+	for(c = 0; c < (ifc.ifc_len/sizeof (struct ifreq)); c++) {
+		cifr = &ifc.ifc_req[c];
+		if (ioctl(fd, SIOCGIFHWADDR, cifr) == -1) return pusherrno(L, "ioctl(2) error");
+		snprintf(mac, sizeof mac, "%02x:%02x:%02x:%02x:%02x:%02x",
+			(unsigned char)cifr->ifr_hwaddr.sa_data[0],
+			(unsigned char)cifr->ifr_hwaddr.sa_data[1],
+			(unsigned char)cifr->ifr_hwaddr.sa_data[2],
+			(unsigned char)cifr->ifr_hwaddr.sa_data[3],
+			(unsigned char)cifr->ifr_hwaddr.sa_data[4],
+			(unsigned char)cifr->ifr_hwaddr.sa_data[5]);
+		assert(mac[17] == '\0');
+		lua_pushstring(L, mac);
+		lua_setfield(L, -2, cifr->ifr_name);
+	}
+	return 1;
+}
+
+/*
+Show utmp information via getutxent(3)
+@function utmp
+@return information (TABLE)
+*/
+static int Futmp(lua_State *L)
+{
+	struct utmpx *utx = {0};
+	setutxent();
+	lua_createtable(L, 0, 7);
+	while ((utx = getutxent())) {
+		lua_pushstring(L, utx->ut_user);
+		lua_setfield(L, -2, "user");
+		lua_pushstring(L, utx->ut_line);
+		lua_setfield(L, -2, "tty");
+		lua_pushstring(L, utx->ut_host);
+		lua_setfield(L, -2, "hostname");
+		lua_pushnumber(L, (float)utx->ut_tv.tv_sec);
+		lua_setfield(L, -2, "timestamp");
+		lua_pushboolean(L, utx->ut_type & USER_PROCESS);
+		lua_setfield(L, -2, "user_process");
+		lua_pushboolean(L, utx->ut_type & INIT_PROCESS);
+		lua_setfield(L, -2, "init_process");
+		lua_pushboolean(L, utx->ut_type & LOGIN_PROCESS);
+		lua_setfield(L, -2, "login_process");
+	}
+	endutxent();
+	return 1;
+}
+
+static const luaL_Reg F[] =
+{
+	{"uptime", Fuptime},
+	{"loads", Floads},
+	{"mem", Fmem},
+	{"procs", Fprocs},
+	{"sysconf", Fsysconf},
+	{"hostname", Fhostname},
+	{"uname", Funame},
+	{"hostid", Fhostid},
+	{"timezone", Ftimezone},
+	{"mount", Fmount},
+	{"ipaddress", Fipaddress},
+	{"ifaddrs", Fifaddrs},
+	{"utmp", Futmp},
+	{"macaddrs", Fmacaddrs},
+	{NULL, NULL}
+};
+
+LUALIB_API int luaopen_factidC(lua_State *L)
+{
+	luaL_newlib(L, F);
+	return 1;
+}
+
---
+++ vendor/lua/factid.lua
@@ -0,0 +1,251 @@
+-- Module for gathering system "facts"
+-- @module factid
+local string = string
+local util = require"cimicida"
+local sysstat = require"posix.sys.stat"
+local dirent = require"posix.dirent"
+local lib = require"lib"
+local cmd = lib.cmd
+local factid = require"factidC"
+local return_false = { __index = function() return false end }
+local ipairs, pairs, tonumber, next, setmetatable = ipairs, pairs, tonumber, next, setmetatable
+local ENV = {}
+_ENV = ENV
+
+--- Deduce the distro ID from /etc/os-release or /etc/*release.
+-- @return the id as a string (STRING)
+function factid.osfamily ()
+    local id
+    if sysstat.stat("/etc/os-release") then
+        id = util.match_from_file("/etc/os-release", [[^ID=[%p]*(%w+)[%p]*$]])
+    elseif sysstat.stat("/etc/openwrt_release") then
+        id = "openwrt"
+    else
+        id = "unknown"
+    end
+    return id
+end
+
+--- Deduce the distro NAME from /etc/os-release or /etc/*release.
+-- @return the name as a string (STRING)
+function factid.operatingsystem ()
+    local name
+    if sysstat.stat("/etc/os-release") then
+        name = util.match_from_file("/etc/os-release", [[^NAME[%s]*=[%s%p]*([%w]+)]])
+    elseif sysstat.stat("/etc/openwrt_release") then
+        name = "OpenWRT"
+    else
+        name = "unknown"
+    end
+    return name
+end
+
+--- Gather ...
+-- Requires Linux sysfs support.
+-- @return partitions as a partition name and size pair (TABLE)
+function factid.partitions ()
+    local partitions = {}
+    local sysfs = sysstat.stat("/sys/block")
+    if not sysfs or sysstat.S_ISDIR(sysfs.st_mode) == 0 then
+        return nil, "factid.partitions: No sysfs support detected."
+    end
+    for partition in dirent.files("/sys/block/") do
+        if not string.find(partition, "^%.") then
+            local size = tonumber(util.fopen("/sys/block/" .. partition .. "/size" ))
+            partitions[partition] = size*512
+        end
+    end
+    if not next(partitions) then
+        return nil, "factid.partitions: posix.dirent failed."
+    end
+    return partitions
+end
+
+function factid.interfaces ()
+    local ifs = {}
+    for _, y in ipairs(factid.ifaddrs()) do
+        if y.ipv4 then
+            ifs[y.interface] = {}
+            ifs[y.interface]["ipv4"] = y.ipv4
+        elseif y.ipv6 then
+            ifs[y.interface]["ipv6"] = y.ipv6
+        end
+    end
+    return ifs
+end
+
+function factid.aws_instance_id ()
+    local ok, err, res
+    local curl, wget = lib.bin_path("curl"), lib.bin_path("wget")
+    local url = "http://169.254.169.254/latest/meta-data/instance-id"
+    if curl then
+        ok, err, res = cmd[curl]{ url }
+    elseif wget then
+        ok, err, res = cmd[wget]{ "-q", "-O-", url }
+    end
+    if ok then
+        if not next(res.stdout) then
+            return nil, "factid.aws_instance_id: No stdout."
+        end
+        local id = res.stdout[1]
+        if string.len(id) == 0 then
+            return nil, "factid.aws_instance_id: Empty stdout."
+        else
+            return id
+        end
+    else
+        return nil, err
+    end
+end
+
+-- WORK IN PROGRESS
+function factid.gather ()
+    local fact = {}
+    fact.version = "0.1.0"
+
+    do
+        local hostname = factid.hostname()
+        fact.hostname = setmetatable({}, return_false)
+        fact.hostname.string = hostname
+        fact.hostname[hostname] = true
+    end
+
+    do
+        local uniqueid = factid.hostid()
+        fact.uniqueid = setmetatable({}, return_false)
+        fact.uniqueid.string = uniqueid
+        fact.uniqueid[uniqueid] = true
+    end
+
+    do
+        local timezone = factid.timezone()
+        fact.timezone = setmetatable({}, return_false)
+        fact.timezone.string = timezone
+        fact.timezone[timezone] = true
+    end
+
+    do
+        local procs = factid.sysconf().procs
+        procs = tonumber(procs)
+        fact.physicalprocessorcount = setmetatable({}, return_false)
+        fact.physicalprocessorcount.number = procs
+        fact.physicalprocessorcount[procs] = true
+    end
+
+    do
+        local osfamily = factid.osfamily()
+        fact.osfamily = setmetatable({}, return_false)
+        fact.osfamily.string = osfamily
+        fact.osfamily[osfamily] = true
+    end
+
+    do
+        local operatingsystem = factid.operatingsystem()
+        fact.operatingsystem = setmetatable({}, return_false)
+        fact.operatingsystem.string = operatingsystem
+        fact.operatingsystem[operatingsystem] = true
+    end
+
+    do
+        local uname = factid.uname()
+        local kernel = uname.sysname
+        fact.kernel = setmetatable({}, return_false)
+        fact.kernel.string = kernel
+        fact.kernel[kernel] = true
+        local architecture = uname.machine
+        fact.architecture = setmetatable({}, return_false)
+        fact.architecture.string = architecture
+        fact.architecture[architecture] = true
+        -- kernel version information are strings
+        local v1, v2, v3 = string.match(uname.release, "(%d+).(%d+).(%d+)")
+        kernelmajversion = string.format("%d.%d", v1, v2)
+        fact.kernelmajversion = setmetatable({}, return_false)
+        fact.kernelmajversion.string = kernelmajversion
+        fact.kernelmajversion[kernelmajversion] = true
+        kernelrelease = uname.release
+        fact.kernelrelease = setmetatable({}, return_false)
+        fact.kernelrelease.string = kernelrelease
+        fact.kernelrelease[kernelrelease] = true
+        kernelversion = string.format("%d.%d.%d", v1, v2, v3)
+        fact.kernelversion = setmetatable({}, return_false)
+        fact.kernelversion.string = kernelversion
+        fact.kernelversion[kernelversion] = true
+    end
+
+    do
+        -- uptime table values are integers
+        -- fields: days, hours, totalseconds, totalminutes
+        local uptime = factid.uptime()
+        fact.uptime = {}
+        fact.uptime.days = setmetatable({}, return_false)
+        fact.uptime.days.number = uptime.days
+        fact.uptime.days[uptime.days] = true
+        fact.uptime.hours = setmetatable({}, return_false)
+        fact.uptime.hours.number = uptime.hours
+        fact.uptime.hours[uptime.hours] = true
+        fact.uptime.totalseconds = setmetatable({}, return_false)
+        fact.uptime.totalseconds.number = uptime.totalseconds
+        fact.uptime.totalseconds[uptime.totalseconds] = true
+        fact.uptime.totalminutes = setmetatable({}, return_false)
+        fact.uptime.totalminutes.number = uptime.totalminutes
+        fact.uptime.totalminutes[uptime.totalminutes] = true
+    end
+
+    do
+        -- string, number table
+        -- { sda = 500000 }
+        local partitions = factid.partitions()
+        if partitions then
+            fact.partitions = {}
+            fact.partitions.table = partitions
+            for p, s in pairs(partitions) do
+                fact.partitions[p] = setmetatable({}, return_false)
+                fact.partitions[p][s] = true
+            end
+        end
+    end
+
+    do
+        -- string, string table
+        -- fields: ipv4, ipv6 default outgoing
+        local ipaddress = factid.ipaddress()
+        fact.ipaddress = {}
+        fact.ipaddress.table = ipaddress
+        for p, i in pairs(ipaddress) do
+            fact.ipaddress[p] = setmetatable({}, return_false)
+            fact.ipaddress[p][i] = true
+        end
+    end
+
+    do
+        -- string, number table
+        -- fields: mem_unit, freehigh, freeswap, totalswap, bufferram, sharedram, freeram, totalram
+        local memory = factid.mem()
+        fact.memory = {}
+        fact.memory.table = memory
+        for k, v in pairs(memory) do
+            fact.memory[k] = setmetatable({}, return_false)
+            fact.memory[k][v] = true
+        end
+    end
+
+    do
+        -- { eth0 = {ipv4=, ipv6=} }
+        local interfaces = factid.interfaces()
+        fact.interfaces = {}
+        fact.interfaces.table = interfaces
+        for interface, prototbl in pairs(interfaces) do
+            fact.interfaces[interface] = {}
+            for proto, ip in pairs(prototbl) do
+                fact.interfaces[interface][proto] = setmetatable({}, return_false)
+                fact.interfaces[interface][proto][ip] = true
+            end
+        end
+    end
+
+    -- { dir, type, freq, opts, passno, fsname }
+    -- fact.mounts = factid.mount()
+    return fact
+end
+
+return factid
