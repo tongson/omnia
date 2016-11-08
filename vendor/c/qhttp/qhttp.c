278518e9609ccb5a8f0bfd66afbd7ebbaa9a26e2
Eduardo Tongson <propolice@gmail.com>
2016-11-08 12:43:08 +0800

    Initial import of qhttp.

---
+++ vendor/c/qhttp/qhttp.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <auxlib.h>
+
+#define BUFSZ 4096
+#define MAXURI 2048
+
+/***
+  Dumb and minimal HTTP client.
+  @module qhttp
+*/
+
+/***
+Simple HTTP GET to an IP
+@function get
+@string HTTP server IP
+@string URI to request
+@treturn string server reply
+*/
+static int
+get(lua_State *L)
+{
+	const char *ip = luaL_checkstring(L, 1);
+	const char *uri = luaL_checkstring(L, 2);
+	char ip4[15] = {0};
+	int fd, read;
+	struct sockaddr_in target = {0};
+	char buf[BUFSZ] = {0};
+	size_t len;
+	if ((len = strlen(ip)) > 15) {
+		return luaX_pusherror(L, "IP argument cannot exceed 15 characters.");
+	}
+	if ((len = strlen(uri)) > MAXURI) {
+		return luaX_pusherror(L, "URI too long.");
+	}
+	strnmove(ip4, ip, 15);
+	target.sin_family = AF_INET;
+	target.sin_port = htons(80);
+	if (inet_pton(AF_INET, ip4, &target.sin_addr.s_addr) == 0) {
+		return luaX_pusherror(L, "inet_pton(3) error. Invalid IP address.");
+	}
+	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		return luaX_pusherrno(L, "socket(2) error.");
+	}
+	if (connect(fd, (struct sockaddr*)&target, sizeof(target)) != 0) {
+		return luaX_pusherrno(L, "connect(2) error.");
+	}
+
+	ssize_t send_bytes = strlen(uri) + 19;
+	ssize_t sent_bytes;
+	ssize_t total_bytes;
+	if (snprintf(buf, send_bytes, "GET %s HTTP/1.0\r\n\r\n\r\n\r\n", uri) < 0) {
+		return luaX_pusherror(L, "snprintf(2) error.");
+	}
+	while (1) {
+	if ((sent_bytes = send(fd, buf, send_bytes, 0)) < 0) {
+		return luaX_pusherrno(L, "send(2) error.");
+	}
+		total_bytes += sent_bytes;
+		if (total_bytes >= send_bytes) {
+			break;
+		}
+	}
+
+	lua_settop(L, 0);
+	while (1) {
+		bzero_x(buf, BUFSZ);
+		read = recv(fd, buf, sizeof(buf), 0);
+		if (read > 0) {
+			lua_pushlstring(L, buf, read);
+			lua_checkstack(L, 1);
+		} else {
+			break;
+		}
+	}
+
+	close(fd);
+	int n = lua_gettop(L);
+	lua_concat(L, n);
+	return 1;
+}
+
+static const
+luaL_Reg qhttp_funcs[] =
+{
+	{"get", get},
+	{0, 0}
+};
+
+int
+luaopen_qhttp(lua_State *L)
+{
+	luaL_newlib(L, qhttp_funcs);
+	return 1;
+}
