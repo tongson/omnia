a7e871332e8207d058abf2099fcc2ed6db34b9a8
Eduardo Tongson <propolice@gmail.com>
2017-04-20 19:28:11 +0800

    Add header guard.

--- vendor/c/auxlib/auxlib.h
+++ vendor/c/auxlib/auxlib.h
@@ -1,3 +1,5 @@
+#ifndef __AUXLIB_H__
+#define __AUXLIB_H__
 
 /*
  * _x suffix for possible conflicts with other libc symbols
@@ -12,3 +14,5 @@ char *strnmove(char* s1, const char* s2, size_t n);
 void assertion_failed(const char*, int, const char*, const char*) __attribute((noreturn));
 #define LIKELY(x) __builtin_expect(!!(x), 1)
 #define xassert(cond, diag) ((void) (LIKELY(cond) || ((assertion_failed)(__FILE__, __LINE__, #diag, #cond), 0)))
+
+#endif
