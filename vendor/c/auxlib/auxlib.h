b3d776b9eb5ad28b7cc91e39d908433d52647a97
Eduardo Tongson <propolice@gmail.com>
2017-04-21 20:47:44 +0800

    CERT API08-C. Avoid parameter names in a function prototype. I agree with this recommendation. The prototypes without the identifier makes it more readable.

--- vendor/c/auxlib/auxlib.h
+++ vendor/c/auxlib/auxlib.h
@@ -6,13 +6,13 @@
  * luaX_ prefix for Lua specific symbols
  */
 
-void bzero_x(void *ptr, size_t len);
-int assert_bzero_x(unsigned char *, size_t len);
-int luaX_pusherror(lua_State *L, const char *error);
-int luaX_pusherrno(lua_State *L, char *error);
-char *strncpy_x(char* s1, const char* s2, size_t n);
-char *strnmove(char* s1, const char* s2, size_t n);
-void assertion_failed(const char*, int, const char*, const char*) __attribute((noreturn));
+void bzero_x(void *, size_t);
+int assert_bzero_x(unsigned char *, size_t);
+int luaX_pusherror(lua_State *, const char *);
+int luaX_pusherrno(lua_State *, char *);
+char *strncpy_x(char *, const char *, size_t);
+char *strnmove(char *, const char *, size_t);
+void assertion_failed(const char *, int, const char *, const char *) __attribute((noreturn));
 #define LIKELY(x) __builtin_expect(!!(x), 1)
 #define REQUIRE(cond, diag) ((void) (LIKELY(cond) || ((assertion_failed)(__FILE__, __LINE__, #diag, #cond), 0)))
 
