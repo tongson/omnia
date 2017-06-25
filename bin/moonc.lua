8f305bc3588969a016cc30468757ce63c45818a0
Eduardo Tongson <propolice@gmail.com>
2017-06-25 16:03:26 +0800

    Update with proper function names and style.

--- bin/moonc.lua
+++ bin/moonc.lua
@@ -1,26 +1,28 @@
-local Script = arg[1]
-local Lua_Script = arg[2]
+local script = arg[1]
+local lua_script = arg[2]
 
-local Lib = require"cimicida"
-local Parse = require"moonscript.parse"
-local Compile = require"moonscript.compile"
+local lib = require "cimicida"
+local fmt = lib.fmt
+local file = lib.file
+local parse = require "moonscript.parse"
+local compile = require "moonscript.compile"
 
-local String = Lib.fopen(Script)
-if not String then
-   Lib.errorf("ERROR: Error reading %s!", Script )
+local string = file.read_to_string(script)
+if not string then
+   fmt.panic("ERROR: Error reading %s!", script )
 end
 
-local Parse_Tree, Parse_Error = Parse.string(String)
-if not Parse_Tree then
-   Lib.errorf("ERROR: %s\n", Parse_Error)
+local parse_tree, parse_error = parse.string(string)
+if not parse_tree then
+   fmt.panic("ERROR: %s\n", parse_error)
 end
 
-local Compiled_Code, Compile_PosMap_Or_Error, Compile_Error_Position = Compile.tree(Parse_Tree)
-if not Compiled_Code then
-   Lib.errorf("ERROR: %s\n", Compile.format_error(Compile_PosMap_Or_Error, Compile_Error_Position, String))
+local compiled_code, compile_posmap_or_error, compile_error_position = compile.tree(parse_tree)
+if not compiled_code then
+   fmt.panic("ERROR: %s\n", compile.format_error(compile_posmap_or_error, compile_error_position, string))
 end
 
-local Written, Write_Error = Lib.fwrite(Lua_Script, Compiled_Code)
-if not Written then
-   Lib.errorf("ERROR: Failed writing %s: %s\n", Lua_Script, Write_Error)
+local written, write_error = file.write_all(lua_script, compiled_code)
+if not written then
+   fmt.panic("ERROR: Failed writing %s: %s\n", lua_script, write_error)
 end
