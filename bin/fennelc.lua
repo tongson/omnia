51652b17e200ac25e21a839b2593edc792d33b24
Eduardo Tongson <propolice@gmail.com>
2018-03-14 12:41:44 +0800

    Remove blank lines, trailing white space and indention from the transpiled Lua code.

--- bin/fennelc.lua
+++ bin/fennelc.lua
@@ -1,14 +1,30 @@
 local script = arg[1]
 local lua_script = arg[2]
 local lib = require "cimicida"
+local string = lib.string
 local fmt = lib.fmt
 local file = lib.file
 local fennel = require "fennel"
-local string = file.read_to_string(script)
-if not string then
-   fmt.panic("ERROR: Error reading %s!", script )
+local code = file.read_to_string(script)
+if not code then
+  fmt.panic("ERROR: Error reading %s!", script )
 end
-local compiled_code = fennel.compileString(string)
+local compiled_code = fennel.compileString(code)
+local n
+while true do -- remove blank lines
+  compiled_code, n = string.gsub(compiled_code, "(\n)[%s]*\n", "%1")
+  if n == 0 then break end
+end
+while true do -- remove trailing spaces
+  compiled_code, n = string.gsub(compiled_code, "[%s]+(\n)", "%1")
+  if n == 0 then break end
+end
+local x = #(string.line_to_array(compiled_code))
+while true do -- remove indention
+  n = n + 1
+  compiled_code = string.gsub(compiled_code, "\n[%s]+", "\n")
+  if x == n then break end
+end
 compiled_code = compiled_code.."\n"
 local written, write_error = file.write_all(lua_script, compiled_code)
 if not written then
