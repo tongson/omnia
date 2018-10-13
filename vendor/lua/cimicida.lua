714a764da7bf463261de036072485f1b4d9f52d7
Eduardo Tongson <propolice@gmail.com>
2018-10-13 07:41:02 +0000

    cimicida: What a mess. Sorry.

--- vendor/lua/cimicida.lua
+++ vendor/lua/cimicida.lua
@@ -576,15 +576,13 @@ end
 -- [first] begin dump at 16 byte-aligned offset containing 'first' byte
 -- [last] end dump at 16 byte-aligned offset containing 'last' byte
 local hexdump = function(buf, first, last)
-	local function align(n) return math.ceil(n/16) * 16 end
-		for i=(align((first or 1)-16)+1),align(math.min(last or #buf,#buf)) do
-			if (i-1) % 16 == 0 then io.write(string.format('%08X  ', i-1)) end
-				io.write( i > #buf and '   ' or string.format('%02X ', buf:byte(i)) )
-				if i %  8 == 0 then io.write(' ') end
-				if i % 16 == 0 then io.write( buf:sub(i-16+1, i):gsub('%c','.'), '\n' ) end
-      end
-    end
-	end
+  local function align(n) return math.ceil(n/16) * 16 end
+  for i=(align((first or 1)-16)+1),align(math.min(last or #buf,#buf)) do
+    if (i-1) % 16 == 0 then io.write(string.format('%08X  ', i-1)) end
+    io.write( i > #buf and '   ' or string.format('%02X ', buf:byte(i)) )
+    if i %  8 == 0 then io.write(' ') end
+    if i % 16 == 0 then io.write( buf:sub(i-16+1, i):gsub('%c','.'), '\n' ) end
+  end
 end
 
 table.find = t_find
